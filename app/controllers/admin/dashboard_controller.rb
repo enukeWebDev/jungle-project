5ea62e6c9f626d107e1b0aa0c72c8f4f9a05c29a
Roderick Nuque <enuke4@gmail.com>
2022-02-03 19:29:17 +0000

    total counts for both products & categories shows on dashboard page with simple boostrap style

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+
+  #  Implement HTTP basic auth as a way of securing the admin/products resource
+
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
   def show
+    @total_counts_products = Product.count
+    @total_counts_categories = Category.count
   end
 end
