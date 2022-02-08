97f678c6c03547c51cc0e0faf3c75ae7fdbe78f8
Roderick Nuque <enuke4@gmail.com>
2022-02-08 02:06:39 +0000

    complete all test - all 5 test pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,33 +5,58 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     # four fields - name, price, quantity, category
-    category = Category.create(name: 'shoes')
-    @product = category.products.new(
+    it 'should validate successfully when products has all four fields' do
+      category = Category.create(name: 'shoes')
+      @product = category.products.new(
       name: 'Air Jordan 11',
       price: '3500.50',
       quantity: 2,
     )
-
-    it 'should validate successfully when products has all four fields' do
       expect(@product).to be_valid
     end
 
     it 'should fail validation if name field is missing' do
+      category = Category.create(name: 'shoes')
+      @product = category.products.new(
+      name: 'Air Jordan 11',
+      price: '3500.50',
+      quantity: 2,
+    )
       @product.name = nil
       expect(@product).to_not be_valid
     end
 
     it 'should fail validation if price field is missing' do
-      @product.price = nil
+      category = Category.create(name: 'shoes')
+      @product = category.products.new(
+      name: 'Air Jordan 11',
+      category: @category,
+      # price: '3500.50',
+      quantity: 2,
+    )
+      # @product.price = nil
       expect(@product).to_not be_valid
     end
 
+
     it 'should fail validation if quantity field is missing' do
+      category = Category.create(name: 'shoes')
+      @product = category.products.new(
+      name: 'Air Jordan 11',
+      price: '3500.50',
+      quantity: 2,
+    )
       @product.quantity = nil
       expect(@product).to_not be_valid
     end
 
     it 'should fail validation if category field is missing' do
+      category = Category.create(name: 'shoes')
+      @product = category.products.new(
+      name: 'Air Jordan 11',
+      price: '3500.50',
+      quantity: 2,
+    )
       @product.category = nil
       expect(@product).to_not be_valid
     end
